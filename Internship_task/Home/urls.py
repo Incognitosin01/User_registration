9cd38e1c9c6a3e66a3d632a339dbd4450a5dd8f9
sumit mahajan <ssmahajan@mitaoe.ac.in>
2021-06-02 17:53:44 +0530

    User registration and basic routing completed

--- Internship_task/Home/urls.py
+++ Internship_task/Home/urls.py
@@ -1,10 +1,14 @@
 from django.urls import path
 # Import Views
-from .views import login
+
+from .import views
 
 app_name = 'Home'
 
 urlpatterns = [
-    path('login/', login),
+    path('',views.user_reg),
+    path('login/', views.login),
+    path('profile/', views.profile),
+    path('user_application/',views.user_app)
     # Add your url config below
 ]
--- Internship_task/Home/views.py
+++ Internship_task/Home/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
 
 
+def user_reg(request):
+    return render(request, 'html/Registration_Page.html')
+
 def login(request):
     response = "Hello, world"
     return render(request, 'html/login.html', {'var': response})
+
+def profile(request):
+    return render(request,'HTML/index.html')
+
+def user_app(request):
+    return render(request,'html/application.html')
