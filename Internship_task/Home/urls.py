7d0779768c38dddfda8914d1eca7a76036f45ee9
cypherxx <65460055+cypherxx@users.noreply.github.com>
2021-06-02 23:11:21 +0530

    applications html added

--- Internship_task/Home/urls.py
+++ Internship_task/Home/urls.py
@@ -6,9 +6,9 @@ from .import views
 app_name = 'Home'
 
 urlpatterns = [
-    path('',views.user_reg),
-    path('login/', views.login),
-    path('profile/', views.profile),
-    path('user_application/',views.user_app)
+    path('',views.user_reg, name="registration"),
+    path('login/', views.login, name="login"),
+    path('profile/', views.profile, name="profile"),
+    path('user_application/',views.user_app, name="application")
     # Add your url config below
 ]
