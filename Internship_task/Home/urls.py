48fb52e5dc6123edcfacece66175ed94e520a130
sumit mahajan <ssmahajan@mitaoe.ac.in>
2021-06-03 11:34:24 +0530

    Phone number fielded updated

--- Internship_task/Home/urls.py
+++ Internship_task/Home/urls.py
@@ -6,8 +6,8 @@ from .import views
 app_name = 'Home'
 
 urlpatterns = [
-    path('',views.user_reg, name="registration"),
-    path('login/', views.login, name="login"),
+    path('',views.user_reg, name="register"),
+    path('login/', views.login, name="log"),
     path('profile/', views.profile, name="profile"),
     path('user_application/',views.user_app, name="application")
     # Add your url config below
