ae1c980f6a6863efe62eece9fcdb13e3fe03d298
Mukesh Gurpude <mukeshgurpude02@gmail.com>
2021-06-03 12:05:38 +0530

    model

--- Internship_task/Home/models.py
+++ Internship_task/Home/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from phonenumber_field.modelfields import PhoneNumberField
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Application(models.Model):
+  user = models.ForeignKey(User, on_delete=models.CASCADE)
+  contact = PhoneNumberField()
+  address = models.TextField(max_length=250)
+
+  resume = models.FileField()
+  aadhar = models.FileField()
+  Marksheet = models.FileField()
+
+  def __str__(self):
+    return f"{self.user.get_full_name()}"
