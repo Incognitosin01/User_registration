b6a0e22be51faee72a972f86f83c8a12d1449fe2
Mukesh Gurpude <mukeshgurpude02@gmail.com>
2021-06-03 13:49:10 +0530

    email verification added

--- Internship_task/Home/urls.py
+++ Internship_task/Home/urls.py
@@ -9,6 +9,7 @@ urlpatterns = [
     path('',views.Registration.as_view(), name="register"),
     path('login/', views.login, name="log"),
     path('profile/', views.profile, name="profile"),
-    path('user_application/',views.user_app, name="application")
-    # Add your url config below
+    path('user_application/',views.user_app, name="application"),
+    path('verify/',views.verify, name="verify"),
+    path('test/',views.test, name="test")
 ]
--- Internship_task/Home/views.py
+++ Internship_task/Home/views.py
@@ -4,31 +4,57 @@ from django.contrib.auth.models import User
 from .models import Application
 from django.core.mail import send_mail
 from django.shortcuts import redirect
+from django.conf import settings
+from django.urls import reverse
+from django.contrib import messages
 import hashlib
-
+from django.core.mail import EmailMultiAlternatives
+from django.template.loader import render_to_string
+from django.utils.html import strip_tags
 
 class Registration(View):
     def get(self, request):
         return render(request, 'html/Registration_Page.html')
 
     def post(self, request):
-        first_name = request.post['first_name']
-        last_name = request.post['last_name']
-        phone_number = request.post['phone_number']
-        password = request.post['password']
-        email = request.post['email']
+        first_name = request.POST['first_name']
+        last_name = request.POST['last_name']
+        phone_number = request.POST['phone']
+        password = request.POST['password']
+        email = request.POST['email']
 
-        user = User.objects.create_user(first_name=first_name, last_name=last_name, 
+        user = User.objects.create_user(username=phone_number, first_name=first_name, last_name=last_name, 
                                         password=password, email=email)
+        user.is_active = False
         user.save()
 
         hash_object = hashlib.md5(email.encode())
         hashed_string = hash_object.hexdigest()
-        
-        send_mail('[important] Email verification',f'Click on the below link to complete your registration
-        {hashed_string}','from@example.com', [user.email],  fail_silently=False,)
 
+        subject, from_email, to = 'Verification', settings.EMAIL_HOST_USER, user.email
+        html_content = render_to_string('html/verification_template.html', {'key': hashed_string, 'email': user.email})
+        text_content = strip_tags(html_content)
+        msg = EmailMultiAlternatives(subject, text_content, from_email, [to])
+        msg.attach_alternative(html_content, "text/html")
+        msg.send()
         return redirect("/")
+
+def verify(request):
+    email = request.GET['email']
+    key = request.GET['key']
+    if hashlib.md5(email.encode()).hexdigest() == key:
+        messages.success(request, 'Verification successfull', 'success')
+        user = User.objects.get(email=email)
+        user.is_active = True 
+        user.save()
+        return redirect(reverse('Home:log'))
+    messages.error(request, 'Verification failed', 'error')
+    return redirect('/')
+
+def test(request):
+    hashed_string = "bkdshhfsbksb"
+    email = "msdfv@gmail.com"
+    return render(request, 'html/verification_template.html', {'key': hashed_string, 'email': email})
 
 def login(request):
     return render(request, 'html/login.html')
