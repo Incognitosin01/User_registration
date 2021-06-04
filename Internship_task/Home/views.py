67bff6b333d04093ae873c2fe3689e1b69d14750
Mukesh Gurpude <mukeshgurpude02@gmail.com>
2021-06-04 18:24:04 +0530

    fix twilio

--- Internship_task/Home/views.py
+++ Internship_task/Home/views.py
@@ -136,7 +136,8 @@ class VerifyOTP(View):
         
         otp = str(req.json()['code'])
         message = f"Your one time password for login is {otp}"
-        self.twilio_client.messages.create(to=phone_number, from_=self.sender, body=message)
+
+        # self.twilio_client.messages.create(to=f"+{phone_number}", from_=self.sender, body=message)
         return JsonResponse({'status': 201, 'message': 'OTP sent successfully', 'otp': otp})
 
     def post(self, request):
